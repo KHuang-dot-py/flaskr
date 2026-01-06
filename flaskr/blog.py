10bee12e62245ee1a190d695a0c2c622c898f375
KHuang-dot-py <kaiwen.huang97@outlook.com>
2026-01-05 20:42:38 -0500

    added update and delete views, update html template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -47,3 +47,61 @@ def create():
 
     return render_template('blog/create.html')
 
+def get_post(id, check_author = True):
+    '''
+    Get and return a post from the db
+    
+    :param id: post id
+    :param check_author: optionally check if viewer is author
+    '''
+    db = get_db
+    post = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        'FROM post p join user ON p.author_id = user.id'
+        'WHERE p.id = ?',
+        (id,) # single-element tuple
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+    
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403) #403 means forbidden, as you are not the author
+
+    return post
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    # implicit return update template for get request
+    return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
