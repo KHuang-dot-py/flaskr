0f31a075a72b486cf177877321a0b03b59e4abdd
KHuang-dot-py <kaiwen.huang97@outlook.com>
2026-01-11 15:24:53 -0500

    Added click.echo statements to print to terminal when this command is run

--- flaskr/cli.py
+++ flaskr/cli.py
@@ -4,5 +4,7 @@ from flask import current_app
 @click.command("migrate")
 def migrate_command():
     """Run database migrations."""
+    click.echo("Running migrations")
     from flaskr.migrate import run_migrations
     run_migrations()
+    click.echo("Migrations completed")
